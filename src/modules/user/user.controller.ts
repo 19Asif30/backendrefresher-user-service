4d84c36617db4d7195875d4f47e85779de5fb5a6
19Asif30 <19asifani30@gmail.com>
2022-12-27 22:47:51 +0530

    crud operation

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,21 +1,58 @@
-/* eslint-disable prettier/prettier */
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { UserDto } from 'src/submodules/backend-refresher-1.0-dtos/src/dtos/user.dto';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { UserDto } from 'src/submodules/backendrefresher-1.0-dtos/src/dtos/user.dto';
 import { UserService } from './user.service';
 
-@Controller('user') 
+@Controller('user')     // .   localhost:3000/user or localhost:3000/user/fetch
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Post()
-  async createUser(@Body()user: UserDto){   
-     try{
-       console.log("Control is here",user)
-     }
-     catch(err){
-       console.log(err);
-       return err;
-     } 
+  async createUser(@Body() user: UserDto) {     // dtos -> data transfer object
+    try {
+      let createdUser = await this.userService.createUser(user);
+      return createdUser;
+    }
+    catch (err) {
+      console.log(err);
+      return err;
+    }
+  }
+
+  @Get()
+  async findUser() {
+    try {
+      let fetchedUser = await this.userService.findAll();
+      return fetchedUser;
+    }
+    catch (err) {
+      console.log(err);
+      return err;
+    }
+  }
+
+  @Put()
+  async updateUser(@Body() user: UserDto) {
+    try {
+      let updateResult = await this.userService.updateUser(user);
+      return updateResult;
+    }
+    catch (err) {
+      console.log(err);
+      return err;
+    }
+
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') userId: number) {
+    try {
+      let deletedUser = await this.userService.deleteUser(userId);
+      return deletedUser;
+    }
+    catch (err) {
+      console.log(err);
+      return err;
+    }
   }
 
 }
